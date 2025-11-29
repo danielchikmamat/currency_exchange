42b588dd69b51f1840ee6f11ff6a877ddad8fcbe
Daniel Chikmamat <54552668+danielchikmamat@users.noreply.github.com>
2025-11-29 13:25:10 +0100

    convert function
    fasapi convert

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ uvicorn main:app --reload
 
 ## request item
 write in the url http://127.0.0.1:8000/items/itemid
+
+## gitignore tips
+do not upload idea files which are local configurations
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import httpx
+
 app = FastAPI()
 
 
@@ -16,4 +18,13 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.post("/convert/{source}/{target}/{amount}")
 def convert(source: str, target: str, amount: float):
-    return {"source": source, "target": target, "exchanged amount": amount*9}
+    #https://api.riksbank.se/swea/v1/CrossRates/{seriedId1}/{seriesId2}/{from}
+    #SEKETT, SEKEURPMI, SEKUSDPMI
+    url = "https://api.riksbank.se/swea/v1/CrossRates/SEKETT/SEKUSDPMI/2025-11-28"
+    response = httpx.get(url) #response object
+    print(response)
+    print(response.json())
+    exchange_rate = response.json()[0]["value"]
+    return {"source": source, "target": target, "exchanged_amount": amount*exchange_rate,
+            "exchange_rate": exchange_rate, "original_amount": amount}
+
